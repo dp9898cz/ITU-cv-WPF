bea6978a2993e700d91942375a6d543720e53e4e
Daniel <xpatek08@stud.fit.vutbr.cz>
2020-11-09 21:01:50 +0100

    completed

---
+++ ITU-Lab-Starter/Windows/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using WPFWeather.Services;
+using WPFWeather.ViewModels;
+
+namespace WPFWeather.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WeatherViewModel viewmodel = new WeatherViewModel(new WeatherDiskService());
+            DataContext = viewmodel;
+        }
+    }
+}
